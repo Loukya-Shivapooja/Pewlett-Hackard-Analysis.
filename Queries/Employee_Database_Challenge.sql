be34a50a28a6787dd20b730cd303a5c59c2a2970
Loukya Munugala <loukyamunugala@raghu-mac.local>
2022-07-31 22:56:10 -0700

    updateing Challenge.

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,6 +1,6 @@
-MODULE CHALLENGE
-Deliverable 1
--- employees titles
+-- Deriverable 1 
+
+employees titles
 SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
 INTO retirement_titles
 FROM employees AS e
@@ -10,11 +10,29 @@ WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (rt.emp_no) rt.emp_no, 
-        rt.first_name, 
-        rt.last_name, 
-        rt.title
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no, rt.first_name, rt.last_name, rt.title
 INTO "Unique Titles"
 FROM retirement_titles AS rt
 WHERE rt.to_date = '9999-01-01'
 ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- Retirement table
+SELECT COUNT(u.title), u.title
+INTO "Retiring Titles"
+FROM "Unique Titles" AS u
+GROUP BY u.title
+ORDER BY COUNT(u.title) DESC;
+
+-- Deliverable 2
+-- Mentorship Eligibility
+SELECT DISTINCT ON(e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date,
+	de.from_date, de.to_date, t.title
+--INTO mentorship_eligibility	
+FROM employees AS e
+JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+JOIN titles AS t
+ON(e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-01')
+ORDER BY e.emp_no;
--- schema.sql
+++ schema.sql
@@ -48,12 +48,3 @@ CREATE TABLE titles (
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     PRIMARY KEY (emp_no, title, from_date)
 );
-MODULE CHALLENGE
--- employees titles
-SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
-INTO retirement_titles
-FROM employees AS e
-JOIN titles AS t
-ON (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-ORDER BY e.emp_no
